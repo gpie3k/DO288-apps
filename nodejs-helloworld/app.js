57387e1a344b3a0d40b791d31dd52842b4b562e6
Pietrzyk, Grzegorz <Grzegorz.Pietrzyk@sabre.com>
2019-09-14 09:51:57 +0200

    added gp message

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+
+app.get('/gp', function (req, res) {
+  res.send('Hello GP!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
